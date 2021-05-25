d92197fc257aa65ede03186550c76b162eb73d47
michellebakels <michelle.bakels@gmail.com>
2021-05-25 08:43:51 -0400

    placeholder landing page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="text-container">
+            <h1 className="neonText">React Miami</h1>
+            <h2 className="neonText">Coming 2022</h2>
+        </div>
       </header>
     </div>
   );
